52134a534e5b1d8672701db01f39d8de27a05f75
chengjieyu77 <chengjieyu01@gmail.com>
2024-06-01 14:45:04 +0900

    add menu function

--- ViewTop10Download/app/src/main/java/com/example/viewtop10download/MainActivity.java
+++ ViewTop10Download/app/src/main/java/com/example/viewtop10download/MainActivity.java
@@ -4,10 +4,15 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,9 +25,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listApps;
+    private String feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
+    private String feedCachedUrl = "INVALIDATED";
+    public static final String STATE_URL = "feedUrl";
+    public static final String STATE_LIMIT = "feedLimit";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -37,8 +48,68 @@ public class MainActivity extends AppCompatActivity {
         });
         listApps = (ListView) findViewById(R.id.xmlListView);
 
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        if (savedInstanceState != null){
+            feedUrl = savedInstanceState.getString(STATE_URL);
+            feedLimit = savedInstanceState.getInt(STATE_LIMIT);
+        }
+
+        downloadUrl(String.format(feedUrl,feedLimit));
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(STATE_URL,feedUrl);
+        outState.putInt(STATE_LIMIT,feedLimit);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feeds_menu,menu);
+        if(feedLimit == 10){
+            menu.findItem(R.id.menu10).setChecked(true);
+        } else if (feedLimit == 25) {
+            menu.findItem(R.id.menu25).setChecked(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuFree){
+            feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+        } else if (id == R.id.menuFree) {
+            feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml";
+        } else if (id == R.id.menuSongs) {
+            feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml";
+        } else if (id == R.id.menu10 || id == R.id.menu25) {
+            if (!item.isChecked()){
+                item.setChecked(true);
+                feedLimit = 35 - feedLimit;
+            }
+        } else if (id == R.id.menuRefresh) {
+            //feedUrl = feedCachedUrl;
+            feedCachedUrl = "INVALIDATED";
+        } else{
+            return super.onOptionsItemSelected(item);
+        }
+
+        downloadUrl(String.format(feedUrl,feedLimit));
+        return true;
+
+    }
+
+    private void downloadUrl(String feedUrl){
+        if (!feedUrl.equalsIgnoreCase(feedCachedUrl)){
+            DownloadData downloadData = new DownloadData();
+            downloadData.execute(feedUrl);
+            feedCachedUrl = feedUrl;
+        }
 
     }
 
@@ -64,10 +135,13 @@ public class MainActivity extends AppCompatActivity {
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
 
-            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(
-                    MainActivity.this,R.layout.list_item,parseApplications.getApplications()
-            );
-            listApps.setAdapter(arrayAdapter);
+//            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(
+//                    MainActivity.this,R.layout.list_item,parseApplications.getApplications()
+//            );
+//            listApps.setAdapter(arrayAdapter);
+            FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this,R.layout.list_record,
+                    parseApplications.getApplications());
+            listApps.setAdapter(feedAdapter);
         }
 
         private String downloadXML(String uriPath){
